459b62f606750b27d1edc059dce517bb71831848
Pieter Koornhof <koornhof.pieter@gmail.com>
2018-10-05 12:58:52 +0200

    Chore: Complete cljs render

--- src/sneakycode/cljs.clj
+++ src/sneakycode/cljs.clj
@@ -1,5 +1,6 @@
 (ns sneakycode.cljs
-  (:require [clojure.java.shell :as sh]))
+  (:require [clojure.java.shell :as sh]
+            [clojure.string :as string]))
 
 
 (def source-proj "resources/inline-cljs")
@@ -11,17 +12,36 @@
 (defn- add-ns [s]
   (str "(ns " out-ns ")\n\n" s))
 
-(defn compile-cjs-str [s]
-  (let [code (add-ns s)]
-    (spit source-path code)
-    (let [compile-result
-          (sh/sh "clj" "--main" "cljs.main" "--optimizations" "advanced" "-c" out-ns
-                 :dir source-proj
-                 )]
-      (when-not (= 0 (:exit compile-result))
-        (throw (ex-info "CLJS Compilation Failed" compile-result))))
-    (slurp dest-path)))
+
+(defn compile-cljs-str
+  ([s] (compile-cljs-str "--optimizations advanced" s))
+  ([opt-str s]
+   (let [code (add-ns s)]
+     (spit source-path code)
+     (let [opt (string/split opt-str #"\h")
+           options (concat
+                    ["clj" "--main" "cljs.main"]
+                    opt
+                    ["-c" out-ns :dir source-proj])
+           compile-result (apply sh/sh options)]
+       (when-not (= 0 (:exit compile-result))
+         (throw (ex-info "CLJS Compilation Failed" compile-result))))
+     (slurp dest-path))))
+
+
+
+(defmacro compile-cljs [& body]
+  (->> body
+       (map str)
+       (string/join "\n\n")
+       compile-cljs-str))
 
 
 (comment
-  (compile-cjs-str "(+ 1 2)"))
+  (compile-cjs-str "(+ 1 2)")
+
+  (compile-cljs
+   (fn [a] a)
+   (prn 1234))
+
+  )
