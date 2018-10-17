0b5166b887d838f143cf73ae9f507c7c224dbd76
Pieter Koornhof <koornhof.pieter@gmail.com>
2018-10-17 11:02:16 +0200

    Chore: Prep minesweeper blog

--- src/sneakycode/cljs.clj
+++ src/sneakycode/cljs.clj
@@ -5,17 +5,17 @@
 
 
 (defmacro compile-cljs
-  [slug namespace-str & body]
-  (let [not-optimize? (conf/getv :js-include-goog?)
-        file-name #(str "/out" (if (string/starts-with? % "/") % (str "/" %)) ".js")]
-    `(do
-       (build/build '[~@body]
-                    {:optimizations (if ~not-optimize? :none :advanced)
-                     :main (when ~not-optimize? ~namespace-str)
-                     :asset-path "out"
-                     :output-dir "inline-cljs/out"
-                     :output-to (str "inline-cljs/" (~file-name ~slug))})
-       (vec
-        [:div
-         (if ~not-optimize? [:script {:type "text/javascript" :src (conf/url "/out/goog/base.js")}] [:span])
-         [:script {:type "text/javascript" :src (conf/url (~file-name ~slug))}]]))))
+  ([optimize? slug namespace-str & body]
+   (let [not-optimize? (and (conf/getv :js-include-goog?) (not optimize?))
+         file-name #(str "/out" (if (string/starts-with? % "/") % (str "/" %)) ".js")]
+     `(do
+        (build/build '[~@body]
+                     {:optimizations (if ~not-optimize? :none :advanced)
+                      :main (when ~not-optimize? ~namespace-str)
+                      :asset-path "out"
+                      :output-dir "inline-cljs/out"
+                      :output-to (str "inline-cljs/" (~file-name ~slug))})
+        (vec
+         [:div
+          (if ~not-optimize? [:script {:type "text/javascript" :src (conf/url "/out/goog/base.js")}] [:span])
+          [:script {:type "text/javascript" :src (conf/url (~file-name ~slug))}]])))))
