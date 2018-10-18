bb7692b67ad2184881d4982b78bd3aa0ddf2ab3a
Pieter Koornhof <koornhof.pieter@gmail.com>
2018-10-18 20:52:23 +0200

    moar content and improvements

--- src/sneakycode/render.clj
+++ src/sneakycode/render.clj
@@ -5,16 +5,27 @@
             [zprint.core :as zp]
             [clojure.java.io :as io]))
 
-(def default-clj-render-opts {:binding {:force-nl? true}})
+(def default-clj-render-opts {:binding {:force-nl? true}
+                              :map {:comma? false}})
 
-(defmacro clj [& body]
+(defn- deep-merge [v & vs]
+  (letfn [(rec-merge [v1 v2]
+            (if (and (map? v1) (map? v2))
+              (merge-with deep-merge v1 v2)
+              v2))]
+    (if (some identity vs)
+      (reduce #(rec-merge %1 %2) v vs)
+      (last vs))))
+
+
+(defmacro clj [options & body]
   (let [start "<pre><code class=\"clojure\">"
         end   "</code></pre>"]
     (str
      start
      "\r\n"
      (->> body
-          (map #(zp/zprint-str % default-clj-render-opts))
+          (map #(zp/zprint-str % (deep-merge default-clj-render-opts options)))
           (string/join "\r\n\r\n"))
      "\r\n"
      end)))
