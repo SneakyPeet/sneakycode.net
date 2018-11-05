b23da824a2332668c61ad0e3db8246662c678d9a
Pieter Koornhof <koornhof.pieter@gmail.com>
2018-11-05 14:20:38 +0200

    Fix: source

--- src/sneakycode/render.clj
+++ src/sneakycode/render.clj
@@ -2,38 +2,16 @@
   (:require [me.raynes.cegdown :as md]
             [clojure.string :as string]
             [clojure.test :refer [function?]]
-            [zprint.core :as zp]
             [clojure.java.io :as io]))
 
-(def default-clj-render-opts {:binding {:force-nl? true}
-                              :map {:comma? false}
-                              :parse {:interpose "\n\n"}})
 
-(defn- deep-merge [v & vs]
-  (letfn [(rec-merge [v1 v2]
-            (if (and (map? v1) (map? v2))
-              (merge-with deep-merge v1 v2)
-              v2))]
-    (if (some identity vs)
-      (reduce #(rec-merge %1 %2) v vs)
-      (last vs))))
-
-
-(defn- render-clj [options s]
-  (-> s
-      (zp/zprint-str (deep-merge default-clj-render-opts options))
-      (string/replace "<" "&lt;")))
-
-
-(defmacro clj [options & body]
+(defn clj [body]
   (let [start "<pre><code class=\"clojure\">"
         end   "</code></pre>"]
     (str
      start
      "\r\n"
-     (->> body
-          (map #(render-clj options %))
-          (string/join "\r\n\r\n"))
+     (string/replace body "<" "&lt;")
      "\r\n"
      end)))
 
